a8cb98a8e175bc266f54b0317502ed3ce535b76a
anusmrith <149612959+Anusmrith@users.noreply.github.com>
2026-09-07 21:42:03 +0530

    Delete attendance calculation function
    Remove calculateAttendance function and its usage.

--- script2.js
+++ script2.js
@@ -1,9 +1,1 @@
-function calculateAttendance(attended, total) {
-    let percentage = (attended / total) * 100;
-    
-  
-    return percentage;
-}
-    let attendance = calculateAttendance(61, 75);
 
-console.log(attendance);
